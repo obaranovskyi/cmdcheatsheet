1b96657bc5dc7bbf9942a432b808688be6c75ab7
Oleg <oleh.baranovskyi.dev.acc@gmail.com>
2022-11-22 19:01:44 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -7,15 +7,12 @@ curr_location = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(curr_location, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.9'
-DESCRIPTION = 'Terminal commands cheat sheet helper'
-
 setup(
     name="cmdcheatsheet",
-    version=VERSION,
+    version='0.0.10',
     author="obaranovskyi (Oleh Baranovskyi)",
     author_email="<oleh.baranovskyi.dev.acc@gmail.com>",
-    description=DESCRIPTION,
+    description='Terminal commands cheat sheet helper',
     long_description_content_type="text/markdown",
     long_description=long_description,
     url="https://github.com/obaranovskyi/cmdcheatsheet",
